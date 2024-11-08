d40364541781569cfc80bddfc50583093a1ed8ca
jaseeerr <jaseerhasbulla01@gmail.com>
2024-11-08 11:08:00 +0400

    newDemands1Updated1212239421213

--- client/urls/urls.js
+++ client/urls/urls.js
@@ -1,3 +1,3 @@
-export const SERVER_URL = 'http://localhost:3000'
+// export const SERVER_URL = 'http://localhost:3000'
 
-// export const SERVER_URL = 'https://notebookapi.estateconnect.cloud'
+export const SERVER_URL = 'https://notebookapi.estateconnect.cloud'
