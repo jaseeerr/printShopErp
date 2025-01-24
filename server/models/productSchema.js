f5a0250bee6b4b2184d90692643bf87fd74e3852
jaseeerr <jaseerhasbulla01@gmail.com>
2025-01-24 18:36:19 +0400

    logo12121220w2112

--- server/models/productSchema.js
+++ server/models/productSchema.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const productSchema = new mongoose.Schema({
   name: { type: String, required: true },          // Product Name
   code: { type: String, },          // Product Code
-  category: { type: String, required: true },          // Product Category
+  category: { type: String, required: false },          // Product Category
   costPrice: { type: String, required: false },          // Cost Price
   price: { type: Number, default: 0 },           // Price (as string for flexibility)
   stock: { type: Number, default: 0 },             // Stock quantity
